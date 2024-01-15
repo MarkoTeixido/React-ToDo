b6a44b7f6053976f1de8db8798c9c81ae8d9970d
MarkoTeixido <teixido_marko@outlook.es>
2024-01-15 16:36:31 -0300

    [FEAT] - corrigiendo detalles

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import { Trash2 } from 'lucide-react';
 
 function TodoItem(props){
+    const [isChecked, setChecked] = useState(false);
+
+    const handleCheckboxChange = () => {
+        setChecked(!isChecked);
+    };
     return (
-        <li className='flex justify-between mx-1.5 text-xl font-light'>
-            <input className='w-5 accent-blue-400' type='checkbox'/>
-            <p>{props.text}</p>
+        <li className='flex justify-between mx-1.5 p-3 text-xl font-light border rounded-md'>
+            <input className='w-5 accent-blue-400' type='checkbox' checked={isChecked} onChange={handleCheckboxChange}/>
+            <p className={`capitalize ${isChecked ? 'line-through' : ''}`}>{props.text}</p>
             <span className='text-red-400 hover:text-red-500'><Trash2/></span>
         </li>
     );
