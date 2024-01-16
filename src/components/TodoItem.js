9212378697441a6df47fe7f4eae92007073e0bee
MarkoTeixido <teixido_marko@outlook.es>
2024-01-15 22:48:41 -0300

    [FEAT] - contador de todos terminado

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,7 +9,7 @@ function TodoItem(props){
     };
     return (
         <li className='flex justify-between mx-1.5 p-3 text-xl font-light border rounded-md'>
-            <input className='w-5 accent-blue-400' type='checkbox' onChange={handleCheckboxChange}/>
+            <input className='w-5 accent-blue-400' type='checkbox' checked={props.completed} onChange={handleCheckboxChange}/>
             <p className={`capitalize ${props.completed ? 'line-through' : ''}`} onClick={handleCheckboxChange}>{props.text}</p>
             <span className='text-red-400 hover:text-red-500'><Trash2/></span>
         </li>
