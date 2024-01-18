0237d6e56b5604e79af1fef6ffd4000d150e9132
MarkoTeixido <teixido_marko@outlook.es>
2024-01-18 17:10:20 -0300

    [FEAT] - tailwind details

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -30,7 +30,7 @@ function TodoForm() {
     return (
         <form onSubmit={onSubmit} className='w-3/12 h-1/4 flex flex-col items-center justify-center gap-4 p-4 border rounded-md bg-neutral-50'>
             <label className='z-10 text-center text-lg font-medium'>Crear Una Nueva Tarea</label>
-            <textarea className='px-1 border-2 rounded-sm border-neutral-400' placeholder='Ingrese la tarea' onChange={onChange}></textarea>
+            <textarea className='px-1 border-2 rounded-sm border-neutral-400' placeholder='Ingrese la tarea' onChange={onChange} required></textarea>
             <div className='flex gap-4 p-0'>
                 <button type="button" className='px-2 border rounded-md hover:border-red-400 hover:text-red-500' onClick={onCancel}>Cancelar</button>
                 <button type="submit" className='px-2 border rounded-md hover:border-blue-400 hover:text-blue-500'>Agregar</button>
