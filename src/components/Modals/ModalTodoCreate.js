b0c44dbdc716bab02e070eaa8b93413384bd2785
MarkoTeixido <teixido_marko@outlook.es>
2024-01-18 16:27:34 -0300

    [FEAT] - tailwind details

--- src/components/Modals/ModalTodoCreate.js
+++ src/components/Modals/ModalTodoCreate.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom';
 
 function ModalTodoCreate({ children }) {
   return ReactDOM.createPortal(
-    <div className='absolute w-full border top-0 left-auto h-screen flex flex-col items-center justify-center backdrop-filter backdrop-blur-sm backdrop-brightness-50 bg-neutral-50 bg-opacity-5'>
+    <div className='absolute w-full top-0 left-auto h-screen flex flex-col items-center justify-center backdrop-filter backdrop-blur-sm backdrop-brightness-50 bg-neutral-50 bg-opacity-5'>
       {children}
     </div>,
     document.getElementById('modal')
