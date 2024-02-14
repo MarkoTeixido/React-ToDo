95cb989657467502589106a089e6fadfbd9a72e6
MarkoTeixido <teixido_marko@outlook.es>
2024-02-14 01:37:23 -0300

    [V1] - todo done

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -9,7 +9,7 @@ function TodoSearch(){
 
   return (
       <input className="self-center text-slate-950 px-1 rounded-sm h-9 sm:w-80 " 
-      placeholder="Tarea..." 
+      placeholder="Buscar tarea..." 
       value={searchValue}
       onChange={(event) => {
         setSearchValue(event.target.value);
